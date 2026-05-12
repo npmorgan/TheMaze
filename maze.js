3ef06500e9eecba0f90c11088935b47eb9b5fbbe
npmorgan <51215204+npmorgan@users.noreply.github.com>
2026-05-12 13:55:42 -0700

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,46 +1,117 @@
-const tree = {
-  text: 'Start: Cupcake.',
-  img: 'media/cupcake.jpg',
+const wizardRoom = {
+  text: 'Wizard room',
+  img: 'media/wizard-room.jpg',
   hotspots: [
     {
-      label: 'Investigate the cupcake',
-      region: [35, 35, 30, 30],
+      label: 'Read spell book',
+      region: [20, 20, 40, 50],
       next: {
-        text: 'A goblin bursts out of the cupcake and runs away.',
-        img: 'media/goblin-run.jpg',
+        text: "wizard appears\nHey! don't touch my stuff",
+        img: 'media/wizard-appears.jpg',
         hotspots: [
           {
-            label: 'Chase the goblin',
-            region: [55, 30, 30, 50],
+            label: 'Continue',
+            region: [30, 40, 40, 30],
             next: {
-              text: 'You enter a dark hall.',
-              img: 'media/dark-hall.jpg',
+              text: "oh wow it’s the two headed nonbinary wizard. they’re famous in this part of town",
+              img: 'media/two-headed-wizard.jpg',
               hotspots: [
                 {
-                  label: 'Take the torch',
-                  region: [40, 25, 20, 50],
+                  label: 'Continue',
+                  region: [30, 40, 40, 30],
                   next: {
-                    text: 'The room ahead is illuminated.',
-                    img: 'media/illuminated-room.jpg',
+                    text: "wizard\ndon't worry about it. everybody makes mistakes. anyways what can I help you with?",
+                    img: 'media/wizard-help.jpg',
                     hotspots: [
                       {
-                        label: 'Climb the ladder',
-                        region: [10, 15, 30, 70],
+                        label: "ignore the irony of asking this two headed nonbinary wizard for help finding your baby’s gender?",
+                        region: [10, 20, 80, 30],
                         next: {
-                          text: 'You enter the mouse party room.',
-                          img: 'media/mouse-party.jpg',
+                          text: 'jump down trap door',
+                          img: 'media/trap-door.jpg',
                           hotspots: [
                             {
-                              label: 'Return to illuminated room',
-                              region: [60, 20, 25, 60],
+                              label: 'Go to kitchen',
+                              region: [30, 40, 40, 30],
                               next: {
-                                text: 'Back in the illuminated room.',
-                                img: 'media/illuminated-room.jpg',
+                                text: 'kitchen',
+                                img: 'media/kitchen.jpg',
                                 hotspots: [
                                   {
-                                    label: 'Go through doorway',
-                                    region: [60, 25, 25, 60],
-                                    next: wizardRoom(),
+                                    label: 'Look in fridge',
+                                    region: [5, 20, 40, 60],
+                                    next: {
+                                      text: 'cats eating a big fish',
+                                      img: 'media/cats-fish.jpg',
+                                      hotspots: [
+                                        {
+                                          label: 'Back to kitchen',
+                                          region: [30, 70, 40, 20],
+                                          next: null,
+                                        },
+                                      ],
+                                    },
+                                  },
+                                  {
+                                    label: 'Open cabinet',
+                                    region: [55, 20, 35, 60],
+                                    next: {
+                                      text: 'goblin inside cabinet',
+                                      img: 'media/goblin-cabinet.jpg',
+                                      hotspots: [
+                                        {
+                                          label: "goblin inside cabinet\nLOL I'll never hand over the gender. that is, unless you have any goblin snacks?",
+                                          region: [10, 20, 80, 30],
+                                          next: {
+                                            text: "wizard\nWait! looks like there's some dust on that goblin snack bag.",
+                                            img: 'media/wizard-dust.jpg',
+                                            hotspots: [
+                                              {
+                                                label: 'blow away dust, reveal POISON goblin snack',
+                                                region: [20, 30, 60, 40],
+                                                next: {
+                                                  text: 'oh shit, should we murder this goblin right now?',
+                                                  img: 'media/poison-snack.jpg',
+                                                  hotspots: [
+                                                    {
+                                                      label: 'no just strangle him bart style',
+                                                      region: [20, 40, 60, 30],
+                                                      next: {
+                                                        text: "wizard strangles the goblin bart style\nhand over the baby gender cretin",
+                                                        img: 'media/strangle.jpg',
+                                                        hotspots: [
+                                                          {
+                                                            label: "baby gender pops out of goblin's back pocket, click on it",
+                                                            region: [40, 40, 20, 20],
+                                                            next: {
+                                                              text: 'little animation of the egg opening to reveal the gender congrats, your baby is awesome',
+                                                              img: 'media/egg.jpg',
+                                                              hotspots: [
+                                                                {
+                                                                  label: 'Continue',
+                                                                  region: [30, 70, 40, 20],
+                                                                  next: {
+                                                                    text: 'all the creatures hanging out together with your baby having a good time THE END',
+                                                                    img: 'media/the-end.jpg',
+                                                                    win: true,
+                                                                    winText:
+                                                                      'all the creatures hanging out together with your baby having a good time THE END',
+                                                                  },
+                                                                },
+                                                              ],
+                                                            },
+                                                          },
+                                                        ],
+                                                      },
+                                                    },
+                                                  ],
+                                                },
+                                              },
+                                            ],
+                                          },
+                                        },
+                                      ],
+                                    },
                                   },
                                 ],
                               },
@@ -49,9 +120,105 @@ const tree = {
                         },
                       },
                       {
-                        label: 'Go through doorway',
-                        region: [60, 25, 25, 60],
-                        next: wizardRoom(),
+                        label: 'ignore - dead end',
+                        region: [10, 60, 80, 20],
+                        next: {
+                          text: 'ignore - dead end',
+                          img: 'media/dead-end.jpg',
+                          dead: true,
+                          deathText: 'ignore - dead end',
+                        },
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+    {
+      label: 'wizard closet - no goblins here',
+      region: [65, 20, 25, 60],
+      next: null,
+    },
+  ],
+};
+
+const kitchen = wizardRoom.hotspots[0]
+  .next.hotspots[0]
+  .next.hotspots[0]
+  .next.hotspots[0]
+  .next;
+
+kitchen.hotspots[0].next.hotspots[0].next = kitchen;
+wizardRoom.hotspots[1].next = wizardRoom;
+
+const tree = {
+  text: 'Start: Cupcake',
+  img: 'media/cupcake.jpg',
+  hotspots: [
+    {
+      label: 'goblin in cupcake',
+      region: [30, 30, 40, 40],
+      next: {
+        text: 'goblin runs',
+        img: 'media/goblin-runs.jpg',
+        hotspots: [
+          {
+            label: 'Dark Hall',
+            region: [5, 20, 40, 60],
+            next: {
+              text: 'torch',
+              img: 'media/torch.jpg',
+              hotspots: [
+                {
+                  label: 'Continue',
+                  region: [30, 40, 40, 30],
+                  next: {
+                    text: 'illuminated room',
+                    img: 'media/illuminated-room.jpg',
+                    hotspots: [
+                      {
+                        label: 'ladder',
+                        region: [10, 20, 30, 60],
+                        next: {
+                          text: 'Mouse party room',
+                          img: 'media/mouse-party.jpg',
+                          hotspots: [
+                            {
+                              label: 'Back to illuminated room',
+                              region: [30, 70, 40, 20],
+                              next: null,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        label: 'doorway',
+                        region: [60, 20, 30, 60],
+                        next: {
+                          text: 'creature holding goblin snack',
+                          img: 'media/creature-snack.jpg',
+                          hotspots: [
+                            {
+                              label: 'closeup on goblin snack',
+                              region: [30, 30, 40, 40],
+                              next: {
+                                text: 'take goblin snack',
+                                img: 'media/take-snack.jpg',
+                                hotspots: [
+                                  {
+                                    label: 'doorway',
+                                    region: [30, 40, 40, 30],
+                                    next: wizardRoom,
+                                  },
+                                ],
+                              },
+                            },
+                          ],
+                        },
                       },
                     ],
                   },
@@ -60,13 +227,13 @@ const tree = {
             },
           },
           {
-            label: "Don't chase",
-            region: [5, 35, 30, 40],
+            label: "don't chase",
+            region: [55, 20, 40, 60],
             next: {
-              text: 'You let the goblin escape forever.',
-              img: 'media/dead-end.jpg',
+              text: "don't chase",
+              img: 'media/dont-chase.jpg',
               dead: true,
-              deathText: 'The goblin escapes with the baby gender forever.',
+              deathText: "don't chase",
             },
           },
         ],
@@ -75,178 +242,10 @@ const tree = {
   ],
 };
 
-function wizardRoom() {
-  return {
-    text: 'You enter the wizard room.',
-    img: 'media/wizard-room.jpg',
-    hotspots: [
-      {
-        label: 'Read spell book',
-        region: [20, 20, 30, 50],
-        next: {
-          text:
-            "A wizard appears. 'Hey! Don't touch my stuff.' Oh wow — it's the famous two-headed nonbinary wizard.",
-          img: 'media/wizard.jpg',
-          hotspots: [
-            {
-              label: 'Ask for help',
-              region: [30, 30, 40, 40],
-              next: {
-                text:
-                  "'Don't worry about it. Everybody makes mistakes. Anyways, what can I help you with?'",
-                img: 'media/wizard-talk.jpg',
-                hotspots: [
-                  {
-                    label: 'Ask about your baby gender',
-                    region: [25, 25, 50, 50],
-                    next: {
-                      text:
-                        'The wizard suggests checking the trap door.',
-                      img: 'media/trap-door.jpg',
-                      hotspots: [
-                        {
-                          label: 'Jump down trap door',
-                          region: [35, 40, 30, 30],
-                          next: kitchenRoom(),
-                        },
-                      ],
-                    },
-                  },
-                  {
-                    label: 'Ignore wizard',
-                    region: [5, 70, 30, 20],
-                    next: {
-                      text: 'You awkwardly leave without solving anything.',
-                      img: 'media/dead-end.jpg',
-                      dead: true,
-                      deathText:
-                        'You leave without finding the baby gender.',
-                    },
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      },
-      {
-        label: 'Open wizard closet',
-        region: [65, 20, 20, 60],
-        next: {
-          text: 'Wizard closet. No goblins here.',
-          img: 'media/closet.jpg',
-          hotspots: [
-            {
-              label: 'Return to wizard room',
-              region: [5, 25, 20, 50],
-              next: wizardRoom(),
-            },
-          ],
-        },
-      },
-    ],
-  };
-}
+const illuminatedRoom =
+  tree.hotspots[0]
+    .next.hotspots[0]
+    .next.hotspots[0]
+    .next;
 
-function kitchenRoom() {
-  return {
-    text: 'You land in the kitchen.',
-    img: 'media/kitchen.jpg',
-    hotspots: [
-      {
-        label: 'Look in fridge',
-        region: [10, 20, 30, 60],
-        next: {
-          text: 'Cats are eating a huge fish.',
-          img: 'media/cats.jpg',
-          hotspots: [
-            {
-              label: 'Back to kitchen',
-              region: [5, 30, 20, 40],
-              next: kitchenRoom(),
-            },
-          ],
-        },
-      },
-      {
-        label: 'Open cabinet',
-        region: [60, 20, 25, 60],
-        next: {
-          text:
-            "A goblin is hiding inside. 'LOL I'll never hand over the gender... unless you have any goblin snacks?'",
-          img: 'media/goblin-cabinet.jpg',
-          hotspots: [
-            {
-              label: 'Offer goblin snack',
-              region: [30, 30, 40, 40],
-              next: {
-                text:
-                  "The wizard notices dust on the snack bag. It's POISON goblin snack.",
-                img: 'media/poison-snack.jpg',
-                hotspots: [
-                  {
-                    label: 'Use poison snack',
-                    region: [55, 25, 30, 50],
-                    next: {
-                      text:
-                        'The goblin immediately dies from poison. The baby gender falls out of its pocket.',
-                      img: 'media/goblin-dead.jpg',
-                      hotspots: [
-                        {
-                          label: 'Open the gender egg',
-                          region: [35, 35, 30, 30],
-                          next: {
-                            text:
-                              'The egg opens. Congrats — your baby is awesome.',
-                            img: 'media/gender-reveal.jpg',
-                            win: true,
-                            winText:
-                              'All the creatures celebrate together with your baby. THE END.',
-                          },
-                        },
-                      ],
-                    },
-                  },
-                  {
-                    label: 'Strangle goblin Bart style',
-                    region: [10, 25, 30, 50],
-                    next: {
-                      text:
-                        "The wizard strangles the goblin Bart style. 'Hand over the baby gender, cretin.'",
-                      img: 'media/bart-style.jpg',
-                      hotspots: [
-                        {
-                          label: 'Take the baby gender',
-                          region: [40, 35, 25, 30],
-                          next: {
-                            text:
-                              'The baby gender pops out of the goblin’s back pocket.',
-                            img: 'media/gender-egg.jpg',
-                            hotspots: [
-                              {
-                                label: 'Open the egg',
-                                region: [35, 35, 30, 30],
-                                next: {
-                                  text:
-                                    'The egg opens to reveal the gender. Congrats — your baby is awesome.',
-                                  img: 'media/gender-reveal.jpg',
-                                  win: true,
-                                  winText:
-                                    'All the creatures hang out together with your baby. THE END.',
-                                },
-                              },
-                            ],
-                          },
-                        },
-                      ],
-                    },
-                  },
-                ],
-              },
-            },
-          ],
-        },
-      },
-    ],
-  };
-}
+illuminatedRoom.hotspots[0].next.hotspots[0].next = illuminatedRoom;
